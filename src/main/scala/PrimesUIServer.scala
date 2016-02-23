68dda24c84072dd07be1af1c16000663d8ed535b
crosson.david@gmail.com <crosson.david@gmail.com>
2016-02-23 20:53:43 +0100

    access logs added

--- src/main/scala/PrimesUIServer.scala
+++ src/main/scala/PrimesUIServer.scala
@@ -1,21 +1,23 @@
 import com.twitter.finagle.http.{Request, Response}
 import com.twitter.finatra.http.HttpServer
-import com.twitter.finatra.http.filters.CommonFilters
+import com.twitter.finatra.http.filters.{CommonFilters,AccessLoggingFilter}
 import com.twitter.finatra.http.routing.HttpRouter
 import com.twitter.finatra.logging.filter.{LoggingMDCFilter, TraceIdMDCFilter}
 import com.twitter.finatra.logging.modules.Slf4jBridgeModule
 
 
 
-object PrimesUIServerMain extends PrimesUIServer
+object PrimesUIServerMain extends PrimesUIServer {
+}
 
 class PrimesUIServer extends HttpServer {
   override def modules = Seq(Slf4jBridgeModule)
 
   override def configureHttp(router: HttpRouter) {
     router
-//      .filter[LoggingMDCFilter[Request, Response]]
-//      .filter[TraceIdMDCFilter[Request, Response]]
+      .filter[LoggingMDCFilter[Request, Response]]
+      .filter[TraceIdMDCFilter[Request, Response]]
+      .filter[AccessLoggingFilter[Request]]
       .filter[CommonFilters]
       .add[PrimesUIController]
   }
