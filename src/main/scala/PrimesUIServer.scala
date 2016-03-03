94bb2a582e6c8c9bcccd3d611eef3f1109916841
david crosson <david.crosson@orange.com>
2016-03-03 21:56:56 +0100

    tuning + finatra update

--- src/main/scala/PrimesUIServer.scala
+++ src/main/scala/PrimesUIServer.scala
@@ -2,7 +2,7 @@ import com.twitter.finagle.http.{Request, Response}
 import com.twitter.finatra.http.HttpServer
 import com.twitter.finatra.http.filters.CommonFilters
 import com.twitter.finatra.http.routing.HttpRouter
-import com.twitter.finatra.logging.filter.{LoggingMDCFilter, TraceIdMDCFilter}
+import com.twitter.finatra.http.filters.{CommonFilters, LoggingMDCFilter, TraceIdMDCFilter}
 import com.twitter.finatra.logging.modules.Slf4jBridgeModule
 
 
@@ -10,7 +10,7 @@ import com.twitter.finatra.logging.modules.Slf4jBridgeModule
 object PrimesUIServerMain extends PrimesUIServer
 
 class PrimesUIServer extends HttpServer {
-  override def modules = Seq(Slf4jBridgeModule)
+  //override def modules = Seq(Slf4jBridgeModule)
 
   override def configureHttp(router: HttpRouter) {
     router
