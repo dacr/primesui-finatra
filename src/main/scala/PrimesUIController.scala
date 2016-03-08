5fc2fc06c073254eb9cdfeb8253664c1897f2585
crosson.david@gmail.com <crosson.david@gmail.com>
2016-03-09 00:20:36 +0100

    mustache & layout taken into account

--- src/main/scala/PrimesUIController.scala
+++ src/main/scala/PrimesUIController.scala
@@ -2,32 +2,37 @@ import com.twitter.finagle.http.Request
 import com.twitter.finatra.http.Controller
 import com.twitter.finatra.http.Controller
 
-import com.twitter.finatra.request.{QueryParam, RouteParam}
+import com.twitter.finatra.request.{ QueryParam, RouteParam }
 import com.twitter.finatra.response.Mustache
 
 import fr.janalyse.primes._
 import fr.janalyse.primesui._
 
 case class PrimesUIContext(
-  homeUrl: String,
-  checkUrl: String
-)
-
-
+  base: String,
+  checkUrl: String)
 
 @Mustache("index")
 case class IndexView(
-    ctx:PrimesUIContext,
-    engine:PrimesEngine,
-    count:Option[Long],
-    version:String,
-    buildate:String,
-    pversion:String
-    )
+  ctx: PrimesUIContext,
+  engine: PrimesEngine,
+  highestPrime: Option[Long],
+  count: Option[Long],
+  version: String,
+  buildate: String,
+  pversion: String)
 
-case class CheckRequest(@RouteParam num:Long)
+@Mustache("check")
+case class CheckView(
+  ctx:PrimesUIContext,
+  num:Long,
+  value:Option[CheckedValue[Long]],
+  againUrl:Option[String]
+)
 
 
+case class CheckRequest(
+  @RouteParam num: Long)
 
 class PrimesUIController extends Controller {
 
@@ -36,41 +41,33 @@ class PrimesUIController extends Controller {
   def nextInt = rnd.nextInt(10000)
 
   val ctx = PrimesUIContext("/", "/check/")
-  
+
   // -------------------------------------------------------------------------------------------------
   get("/") { request: Request =>
     val count = if (!engine.useSession) None else {
       Some(9999L)
     }
     IndexView(
-        ctx = ctx,
-        engine = engine,
-        count = count,
-        version = MetaInfo.version,
-        buildate = fr.janalyse.primesui.MetaInfo.buildate,
-        pversion = primes.MetaInfo.version
-        )
+      ctx = ctx,
+      engine = engine,
+      count = count,
+      highestPrime = engine.lastPrime.map(_.value),
+      version = MetaInfo.version,
+      buildate = fr.janalyse.primesui.MetaInfo.buildate,
+      pversion = primes.MetaInfo.version)
   }
 
   // -------------------------------------------------------------------------------------------------  
-  def check(num:Long) = {
+  def check(num: Long, againUrl:Option[String]) = {
     val state = engine.check(num)
-    val resp = state match {
-      case Some(r) if r.isPrime =>
-        s"<html><body>$num is the ${r.nth} prime number</body></html>"
-      case Some(r) =>
-        s"<html><body>$num is the ${r.nth} not prime number</body></html>"
-      case None =>
-        s"<html><body>$num primes state is unknown</body></html>"
-    }
-    resp    
+    CheckView(ctx, num, state, againUrl)
   }
-  
-  get("/check") { request: Request => check(nextInt)}
-  get("/check/:num") { request: CheckRequest => check(request.num)}
- 
-  get("/js/:file") { request: Request => response.ok.file("/static/js/"+request.params("file")) }
-  get("/css/:file") { request: Request => response.ok.file("/static/css/"+request.params("file")) }
-  get("/images/:file") { request: Request => response.ok.file("/static/images/"+request.params("file")) }
-  
+
+  get("/check") { request: Request => check(nextInt, Some("check")) }
+  get("/check/:num") { request: CheckRequest => check(request.num, None) }
+
+  // -------------------------------------------------------------------------------------------------  
+  for { res <- List("js", "css", "images") } {
+    get(s"/$res/:file") { request: Request => response.ok.file(s"/static/$res/" + request.params("file")) }
+  }
 }
